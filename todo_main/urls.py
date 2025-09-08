b30a6c3e61d8d1020de45c2b6c0f1fde01bce836
Prabhat Burnwal <prabhat@valuebound.com>
2025-09-08 10:10:11 +0530

    Add task completed.

--- todo_main/urls.py
+++ todo_main/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+
+    # TO DO
+    path('todos/', include('todos.urls'))
 ]
--- todo_main/views.py
+++ todo_main/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from todos.models import Task
+
 def home(request):
-    return render(request, 'home.html')
+    active_tasks = Task.objects.filter(is_completed=False).order_by('-id')
+
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-completed_on')
+
+    context = {
+        'active_tasks': active_tasks,
+        'completed_tasks': completed_tasks
+    }
+    return render(request, 'home.html', context)
